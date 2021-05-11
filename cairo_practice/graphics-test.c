dc21b0a2786fb390273549dac2b56b82d8e2a0cf
Shisket <shiras@khanlabschool.org>
2021-05-11 10:43:08 -0700

    ughhhhh

--- cairo_practice/graphics-test.c
+++ cairo_practice/graphics-test.c
@@ -3,12 +3,20 @@
 #include <stdio.h>
 #include <math.h>
 #include <SDL.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[]) {
   SDL_Surface *sdlsurf = SDL_CreateRGBSurface (0, 500, 500, 32, 0x00FF0000, 0x0000FF00, 0x000000FF, 0);
 
+  SDL_Window *window = SDL_Create_Window ("An SDL2 window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 550, 550, 0);
+
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+
   cairo_surface_t *surface = cairo_image_surface_create_for_data (sdlsurf->pixels, CAIRO_FORMAT_RGB24, sdlsurf->w, sdlsurf->h, sdlsurf->pitch);
   cairo_t *cr;
+
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+  SDL_RenderClear(renderer);
 
   cr = cairo_create (surface);
 
@@ -32,10 +40,40 @@ int main(int argc, char *argv[]) {
   cairo_close_path(cr);
   cairo_fill (cr);
 
+  // cairo_destroy (cr);
+  // cairo_surface_write_to_png (surface, "test.png");
+  // cairo_surface_destroy (surface);
 
-  cairo_destroy (cr);
-  cairo_surface_write_to_png (surface, "test.png");
-  cairo_surface_destroy (surface);
-  printf("Success\n");
+  SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, sdl_surface);
+  SDL_FreeSurface(sdl_surface);
+
+  SDL_RenderCopy(renderer, texture, NULL, NULL);
+  SDL_RenderPresent(renderer);
+
+  bool done = false;
+  while (!done) {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+      switch (event.type) {
+      case SDL_QUIT:
+        done = true;
+        break;
+      default:
+        break;
+      }
+    }
+    SDL_Delay(100);
+  }
+
+  cairo_destroy(cr);
+  cairo_surface_destroy(surface);
+
+  SDL_DestroyTexture(texture);
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+
+  SDL_Quit();
+
+  printf("Success!\n");
   return 0;
 }
