4cc12f29061e1c665a94b380ac683abcbff9ea87
Shisket <shiras@khanlabschool.org>
2021-05-13 10:11:07 -0700

    IT DRAWS THE AXES NOW!!!!

--- graphs.c
+++ graphs.c
@@ -7,7 +7,7 @@
 // #include "download.h"
 // #include <cjson/cJSON.h>
 
-int main(int argc, char *argv[]) {
+int getGraph(float upper_x, float upper_y, char *title) {
   SDL_Surface *sdlsurf = SDL_CreateRGBSurface (0, 800, 600, 32, 0x00FF0000, 0x0000FF00, 0x000000FF, 0);
 
   SDL_Window *window = SDL_CreateWindow ("An SDL2 window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, 0);
@@ -22,7 +22,24 @@ int main(int argc, char *argv[]) {
 
   cr = cairo_create (surface);
 
-  //
+  //Drawing Stuff
+
+  cairo_set_source_rgb (cr, 1, 1, 1);
+  cairo_paint(cr);
+
+  cairo_set_source_rgb (cr, 0, 0, 0);
+
+  cairo_move_to (cr, 100, 75);
+  cairo_select_font_face (cr, "sans_serif", 0, 1);
+  cairo_set_font_size (cr, 32);
+  cairo_show_text (cr, title);
+
+  cairo_move_to (cr, 700, 500);
+  cairo_line_to (cr, 100, 500);
+  cairo_line_to (cr, 100, 100);
+  cairo_stroke (cr);
+
+  //End of Drawing Stuff
 
   SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, sdlsurf);
   SDL_FreeSurface(sdlsurf);
@@ -56,4 +73,8 @@ int main(int argc, char *argv[]) {
 
   printf("Success!\n");
   return 0;
+}
+
+int main(int argc, char *argv[]) {
+  getGraph(0, 0, "AAPL Stock");
 }
