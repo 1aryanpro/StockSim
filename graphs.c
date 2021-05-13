5406980b5b8342c776a67e8d0582a137afcf53ed
Shisket <shiras@khanlabschool.org>
2021-05-13 09:23:16 -0700

    create graphs.c

--- graphs.c
+++ graphs.c
@@ -4,6 +4,8 @@
 #include <math.h>
 #include <SDL.h>
 #include <stdbool.h>
+#include "download.h"
+#include <cjson/cJSON.h>
 
 int main(int argc, char *argv[]) {
   SDL_Surface *sdlsurf = SDL_CreateRGBSurface (0, 500, 500, 32, 0x00FF0000, 0x0000FF00, 0x000000FF, 0);
@@ -20,29 +22,7 @@ int main(int argc, char *argv[]) {
 
   cr = cairo_create (surface);
 
-  cairo_set_source_rgb (cr, 0.9, 0.8, 0.9);
-  cairo_paint(cr);
-
-  cairo_set_source_rgb (cr, 0, 0.5, 0.8);
-
-  cairo_move_to (cr, 10, 10);
-  cairo_line_to (cr, 40, 10);
-  cairo_rel_line_to (cr, -15, 30);
-  cairo_close_path(cr);
-  cairo_fill (cr);
-
-  //make a heart
-  cairo_move_to (cr, 100, 50);
-  cairo_line_to (cr, 120, 75);
-  cairo_line_to (cr, 140, 50);
-  cairo_arc (cr, 130, 50, 10, M_PI, 0);
-  cairo_arc (cr, 110, 50, 10, M_PI, 0);
-  cairo_close_path(cr);
-  cairo_fill (cr);
-
-  // cairo_destroy (cr);
-  // cairo_surface_write_to_png (surface, "test.png");
-  // cairo_surface_destroy (surface);
+  
 
   SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, sdlsurf);
   SDL_FreeSurface(sdlsurf);
